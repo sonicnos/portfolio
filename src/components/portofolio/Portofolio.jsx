51d03aa9794f2d25c0ab96c8818346d04a2ee3c6
Loukas <loukasbass@gmail.com>
2024-01-10 16:03:23 +0100

    "nav menu correction"

--- src/components/portofolio/Portofolio.jsx
+++ src/components/portofolio/Portofolio.jsx
@@ -4,51 +4,51 @@ import IMG1 from '../../assets/portfolio1.jpg'
 import IMG2 from '../../assets/portfolio2.jpg'
 import IMG3 from '../../assets/portfolio3.jpg'
 import IMG4 from '../../assets/portfolio4.jpg'
-import IMG5 from '../../assets/portfolio5.png'
+import IMG5 from '../../assets/portfolio5.jpg'
 import IMG6 from '../../assets/portfolio6.jpg'
 
 const data = [
   {
     id: 1,
     image: IMG1,
-    title: 'Crypto currency Dashboard & Financial Visualization',
-    github: 'http://github.com',
-    demo: 'https://github.com'
+    title: 'BeMovies',
+    github: 'https://github.com/sonicnos/bemovies',
+    demo: 'https://sonicnos.github.io/bemovies/'
   },
   {
     id: 2,
     image: IMG2,
-    title: 'Crypto currency Dashboard & Financial Visualization',
-    github: 'http://github.com',
-    demo: 'https://github.com'
+    title: 'Studio Design Landin Page',
+    github: 'https://github.com/sonicnos/Studio-Design-Landin-Page',
+    demo: 'https://sonicnos.github.io/Studio-Design-Landin-Page/'
   },
   {
     id: 3,
     image: IMG3,
-    title: 'Crypto currency Dashboard & Financial Visualization',
-    github: 'http://github.com',
-    demo: 'https://github.com'
+    title: 'Interactive rating component main',
+    github: 'https://github.com/sonicnos/interactive-rating-component-main',
+    demo: 'https://sonicnos.github.io/interactive-rating-component-main/'
   },
   {
     id: 4,
     image: IMG4,
-    title: 'Crypto currency Dashboard & Financial Visualization',
-    github: 'http://github.com',
-    demo: 'https://github.com'
+    title: 'Product preview card component main front end mentor',
+    github: 'https://github.com/sonicnos/product-preview-card-component-main-front-end-mentor',
+    demo: 'https://sonicnos.github.io/product-preview-card-component-main-front-end-mentor/'
   },
   {
     id: 5,
     image: IMG5,
-    title: 'Crypto currency Dashboard & Financial Visualization',
-    github: 'http://github.com',
-    demo: 'https://github.com'
+    title: '3 column preview card component main',
+    github: 'https://github.com/sonicnos/3-column-preview-card-component-main/',
+    demo: 'https://sonicnos.github.io/3-column-preview-card-component-main/'
   },
   {
     id: 6,
     image: IMG6,
-    title: 'Crypto currency Dashboard & Financial Visualization',
-    github: 'http://github.com',
-    demo: 'https://github.com'
+    title: 'QR code Frontend Mentor',
+    github: 'https://github.com/sonicnos/QR-code-Frontend-Mentor',
+    demo: 'https://sonicnos.github.io/QR-code-Frontend-Mentor/'
   }
 ]
 
