d64084fbd74fdd4150877ef8175b5c4454dfa131
Loukas <loukasbass@gmail.com>
2024-07-27 08:34:07 +0300

    update cv project

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import './contact.css'
-import { useRef } from 'react';
-import emailjs from 'emailjs-com'
+import React from "react";
+import "./contact.css";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
   const form = useRef();
@@ -9,28 +9,53 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_dy5pddd', 'template_7h2x6ss', form.current, 'xEpo1dURXJitTk3QH')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
+    console.log(form.current);
+    console.log("log");
+
+    return;
+    emailjs
+      .sendForm(
+        "service_dy5pddd",
+        "template_7h2x6ss",
+        form.current,
+        "xEpo1dURXJitTk3QH"
+      )
+      .then(
+        (result) => {
+          console.log("ok");
+        },
+        (error) => {
+          console.log("error");
+        }
+      );
   };
 
   return (
-    <section id='contact'>
-    <h5>Get in Touch</h5>
-    <h2>Contact Me</h2>
+    <section id="contact">
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
       <div className="container contact__container">
         <form ref={form} onSubmit={sendEmail}>
-          <input type="text" name="name" placeholder='Your Full Name' required/>
-          <input type="email" name="email" placeholder='Your Email' required/>
-          <textarea name="message" rows="7" placeholder='Your message' required></textarea>
-          <button type="submit" className='btn btn-primary'>Send Message</button>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
         </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/portofolio/Portofolio.jsx
+++ src/components/portofolio/Portofolio.jsx
@@ -1,84 +1,85 @@
-import React from 'react'
-import './portofolio.css'
-import IMG1 from '../../assets/portfolio1.jpg'
-import IMG2 from '../../assets/portfolio2.jpg'
-import IMG3 from '../../assets/portfolio3.jpg'
-import IMG4 from '../../assets/portfolio4.jpg'
-import IMG5 from '../../assets/portfolio5.jpg'
-import IMG6 from '../../assets/portfolio6.jpg'
+import React from "react";
+import "./portofolio.css";
+import IMG1 from "../../assets/portfolio1.jpg";
+import IMG2 from "../../assets/portfolio2.jpg";
+import IMG3 from "../../assets/portfolio3.jpg";
+import IMG4 from "../../assets/portfolio4.jpg";
+import IMG5 from "../../assets/portfolio5.jpg";
+import IMG6 from "../../assets/portfolio6.jpg";
 
 const data = [
   {
     id: 1,
     image: IMG1,
-    title: 'BeMovies',
-    github: 'https://github.com/sonicnos/bemovies',
-    demo: 'https://sonicnos.github.io/bemovies/'
+    title: "Client Portofolio",
+    github: "https://github.com/sonicnos/mariadrac",
+    demo: "https://mdrakopoulou.com/",
   },
   {
     id: 2,
     image: IMG2,
-    title: 'Studio Design Landin Page',
-    github: 'https://github.com/sonicnos/Studio-Design-Landin-Page',
-    demo: 'https://sonicnos.github.io/Studio-Design-Landin-Page/'
+    title: "Next Auth",
+    github: "https://github.com/sonicnos/next-auth-v5",
+    demo: "https://next-auth-v5-mocha.vercel.app/",
   },
   {
     id: 3,
     image: IMG3,
-    title: 'Job Apply tracker',
-    github: 'https://github.com/sonicnos/jobApply-tracker',
-    demo: 'https://job-tracker-eight.vercel.app/'
+    title: "BeMovies",
+    github: "https://github.com/sonicnos/bemovies",
+    demo: "https://sonicnos.github.io/bemovies/",
   },
   {
     id: 4,
     image: IMG4,
-    title: 'Interactive rating component main',
-    github: 'https://github.com/sonicnos/interactive-rating-component-main',
-    demo: 'https://sonicnos.github.io/interactive-rating-component-main/'
+    title: "Job Apply tracker",
+    github: "https://github.com/sonicnos/jobApply-tracker",
+    demo: "https://job-tracker-eight.vercel.app/",
   },
   {
     id: 5,
     image: IMG5,
-    title: 'Product preview card component main front end mentor',
-    github: 'https://github.com/sonicnos/product-preview-card-component-main-front-end-mentor',
-    demo: 'https://sonicnos.github.io/product-preview-card-component-main-front-end-mentor/'
+    title: "Interactive rating component main",
+    github: "https://github.com/sonicnos/interactive-rating-component-main",
+    demo: "https://sonicnos.github.io/interactive-rating-component-main/",
   },
   {
     id: 6,
     image: IMG6,
-    title: 'QR code Frontend Mentor',
-    github: 'https://github.com/sonicnos/QR-code-Frontend-Mentor',
-    demo: 'https://sonicnos.github.io/QR-code-Frontend-Mentor/'
-  }
-]
+    title: "Studio Design Landin Page",
+    github: "https://github.com/sonicnos/Studio-Design-Landin-Page",
+    demo: "https://sonicnos.github.io/Studio-Design-Landin-Page/",
+  },
+];
 
 const Portofolio = () => {
   return (
-    <section id='portofolio'>
+    <section id="portofolio">
       <h5>My Recent Work</h5>
       <h2>Portofolio</h2>
 
       <div className="container portofolio__container">
-        {
-        data.map(({id, image, title, github, demo}) => {
+        {data.map(({ id, image, title, github, demo }) => {
           return (
-          <article key={id} className="portofolio__items">
-          <div className="portofolio__items-image">
-            <img src={image} alt={title} />
-          </div>
-            <h3>{title}</h3>
-            <div className="portofolio__item-cta">
-              <a href={github} className='btn' target='_blank'>Github</a>
-              <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
-            </div>
-          </article>
-          )
-        })
-        }
-
+            <article key={id} className="portofolio__items">
+              <div className="portofolio__items-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portofolio__item-cta">
+                <a href={github} className="btn" target="_blank">
+                  Github
+                </a>
+                <a href={demo} className="btn btn-primary" target="_blank">
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Portofolio
+export default Portofolio;
